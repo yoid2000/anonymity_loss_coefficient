eee3d610d6a42de3fa5adaabc2cd09f7f3b7f2e3
yoid2000 <paul@francis.com>
2025-04-20 14:16:55 +0200

    fix MANIFEST.in for pip install

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='anonymity_loss_coefficient',
-    version='1.0.8',
+    version='1.0.10',
     description='A package to run attacks on anonymized data using the Anonymity Loss Coefficient.',
     author='Paul Francis',
     author_email='paul@francis.com',
