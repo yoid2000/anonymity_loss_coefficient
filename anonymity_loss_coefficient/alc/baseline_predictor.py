452b7bc1571b4627ec9b429c2614ebb91c727f94
Paul Francis <paul@francis.com>
2025-07-28 16:38:39 +0200

    Update baseline_predictor.py

--- anonymity_loss_coefficient/alc/baseline_predictor.py
+++ anonymity_loss_coefficient/alc/baseline_predictor.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.metrics import make_scorer, log_loss
-from analyze_6430 import analyze_6430
+from .analyze_6430 import analyze_6430
 import logging
 import numpy as np
 
